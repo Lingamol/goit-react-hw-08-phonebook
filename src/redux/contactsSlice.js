75ce7ee0e315b5371918618f0d6d51dac2cf07ae
Олег Бакалов <O.O.Bakalov@gmail.com>
2022-11-09 23:05:59 +0200

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,12 +4,7 @@ import { fetchContacts, addContact, deleteContact } from './operations ';
 // import { persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
 const contactsInitialState = {
-  contactList: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contactList: [],
   isLoading: false,
   error: null,
 };
